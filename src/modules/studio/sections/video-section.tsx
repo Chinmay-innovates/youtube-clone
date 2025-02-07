baf6d8a91abd49bab0c770061f8c0b3cd664ee9f
Chinmay-innovates <chinmaygope69@gmail.com>
2025-02-07 20:25:18 +0530

    implement table view for vidoes

--- src/modules/studio/sections/video-section.tsx
+++ src/modules/studio/sections/video-section.tsx
@@ -5,6 +5,15 @@ import { DEFAULT_LIMIT } from "@/constants";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
 
 export const VideoSection = () => {
 	return (
@@ -17,7 +26,7 @@ export const VideoSection = () => {
 };
 
 const VideoSectionSuspense = () => {
-	const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+	const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
 		{
 			limit: DEFAULT_LIMIT,
 		},
@@ -27,7 +36,42 @@ const VideoSectionSuspense = () => {
 	);
 	return (
 		<div>
-			{JSON.stringify(data)}
+			<div className="border-y">
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead className="pl-6 w-[510px]">Vidoes</TableHead>
+							<TableHead>Visibility</TableHead>
+							<TableHead>Status</TableHead>
+							<TableHead>Date</TableHead>
+							<TableHead className="text-right">Views</TableHead>
+							<TableHead className="text-right">Comments</TableHead>
+							<TableHead className="text-right pr-6">Likes</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{videos.pages
+							.flatMap((page) => page.items)
+							.map((video) => (
+								<Link
+									href={`/studio/videos/${video.id}`}
+									key={video.id}
+									legacyBehavior
+								>
+									<TableRow className="cursor-pointer">
+										<TableCell>{video.title}</TableCell>
+										<TableCell>Visibility</TableCell>
+										<TableCell>Status</TableCell>
+										<TableCell>Date</TableCell>
+										<TableCell className="text-right">Views</TableCell>
+										<TableCell className="text-right">comments</TableCell>
+										<TableCell className="text-right pr-6">likes</TableCell>
+									</TableRow>
+								</Link>
+							))}
+					</TableBody>
+				</Table>
+			</div>
 			<InfiniteScroll
 				isManual
 				hasNextPage={query.hasNextPage}
