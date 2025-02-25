723cbea10be9f754740949d9e590183b9152ef2a
Chinmay-innovates <chinmaygope69@gmail.com>
2025-02-25 13:40:13 +0530

    disable caching

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,6 +1,8 @@
 import { DEFAULT_LIMIT } from "@/constants";
 import { HydrateClient, trpc } from "@/trpc/server";
 
+export const dynamic = "force-dynamic";
+
 import { StudioView } from "@/modules/studio/ui/views/studio-view";
 const StudioPage = async () => {
 	void trpc.studio.getMany.prefetchInfinite({
